7f99b785d93e66254b4798da7f5ac0d6d5321943
Benji Fisher <bfisher@isovera.com>
2016-09-25 16:07:55 -0400

    composer: fix coding style, add code comment

--- Formula/composer.rb
+++ Formula/composer.rb
@@ -1,17 +1,6 @@
 require File.expand_path("../../Abstract/abstract-php-phar", __FILE__)
 
 class Composer < AbstractPhpPhar
-  def phar_wrapper
-    <<-EOS.undent
-      #!/usr/bin/env php
-      <?php
-      array_shift($argv);
-      $arg_string = implode(' ', $argv);
-      passthru("/usr/bin/env php -d allow_url_fopen=On -d detect_unicode=Off #{libexec}/#{@real_phar_file} $arg_string", $return_var);
-      return $return_var;
-    EOS
-  end
-
   init
   desc "Dependency Manager for PHP"
   homepage "http://getcomposer.org"
@@ -28,5 +17,20 @@ class Composer < AbstractPhpPhar
 
   test do
     system "composer", "--version"
+  end
+
+  # The default behavior is to create a shell script that invokes the phar file.
+  # Other tools, at least Ansible, expect the composer executable to be a PHP
+  # script, so override this method. See
+  # https://github.com/Homebrew/homebrew-php/issues/3590
+  def phar_wrapper
+    <<-EOS.undent
+      #!/usr/bin/env php
+      <?php
+      array_shift($argv);
+      $arg_string = implode(' ', $argv);
+      passthru("/usr/bin/env php -d allow_url_fopen=On -d detect_unicode=Off #{libexec}/#{@real_phar_file} $arg_string", $return_var);
+      return $return_var;
+    EOS
   end
 end
